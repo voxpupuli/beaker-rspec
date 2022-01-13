voxpupuli/beaker-rspec

commit 37634345b82fa3c133d8ff6c7cf8d373ce84b0f1

Reduce beaker-pe to a development dependendency
In 776f6961725843fce98f6f6b0f18f0926c8a3527 beaker-pe was added as a
runtime dependency, but it is really only needed in the spec/ directory.

Fixes: 776f6961725843fce98f6f6b0f18f0926c8a3527

beaker-rspec.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'thin'
 
   # Dependency for example spec/acceptance tests
-  s.add_runtime_dependency 'beaker-pe'
+  s.add_development_dependency 'beaker-pe'
 
   # Run time dependencies
   s.add_runtime_dependency 'beaker', '> 3.0'

commit 1b40030322d866c46e4bdf6ef1007cf0fbd1e0b5

Drop beaker-pe dependency
This takes 37634345b82fa3c133d8ff6c7cf8d373ce84b0f1 a step further and
completely gets rid of it.

Fixes: 37634345b82fa3c133d8ff6c7cf8d373ce84b0f1

beaker-rspec.gemspec
@@ -28,9 +28,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'yard'
   s.add_development_dependency 'thin'
 
-  # Dependency for example spec/acceptance tests
-  s.add_development_dependency 'beaker-pe'
-
   # Run time dependencies
   s.add_runtime_dependency 'beaker', '> 3.0'
   s.add_runtime_dependency 'rspec', '~> 3.0'

spec/acceptance/example_spec.rb
@@ -12,8 +12,8 @@ describe "ignore" do
     self.hosts = hosts
   end
 
-  example "access options hash" do
-    install_pe
+  example "use DSL method" do
+    echo_on hosts, 'Hello World!'
   end
 
   example "access the logger" do

spec/spec_helper.rb
@@ -1,6 +1,3 @@
-# if you need DSL extension libraries, it appears that they have to be loaded before the RSpec shim
-require "beaker-pe"
-
 ENV['RS_SETFILE'] ||= 'sample.cfg'
 
 require "beaker-rspec"