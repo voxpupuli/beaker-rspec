voxpupuli/beaker-rspec

commit 776f6961725843fce98f6f6b0f18f0926c8a3527

(MAINT) Add beaker-vmpooler for CI pipeline

beaker-rspec.gemspec
@@ -33,6 +33,9 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'yard'
   s.add_development_dependency 'thin'
 
+  # Dependency for example spec/acceptance tests
+  s.add_runtime_dependency 'beaker-pe'
+
   if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.2.2')
     s.add_development_dependency 'rack', '~> 1.6'
   end

Gemfile
@@ -21,4 +21,8 @@ end
 # For running the spec/acceptance/example_spec.rb
 gem 'beaker-vagrant'
 
+# Dependencies for CI acceptance testing; TODO: fix CI so this can be removed
+gem 'beaker-hostgenerator'
+gem 'beaker-vmpooler', '~> 1.3'
+
 gemspec

spec/spec_helper.rb
@@ -1,3 +1,6 @@
+# if you need DSL extension libraries, it appears that they have to be loaded before the RSpec shim
+require "beaker-pe"
+
 ENV['RS_SETFILE'] ||= 'sample.cfg'
 
 require "beaker-rspec"

commit 37634345b82fa3c133d8ff6c7cf8d373ce84b0f1

Reduce beaker-pe to a development dependendency
In 776f6961725843fce98f6f6b0f18f0926c8a3527 beaker-pe was added as a
runtime dependency, but it is really only needed in the spec/ directory.

Fixes: 776f6961725843fce98f6f6b0f18f0926c8a3527

beaker-rspec.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'thin'
 
   # Dependency for example spec/acceptance tests
-  s.add_runtime_dependency 'beaker-pe'
+  s.add_development_dependency 'beaker-pe'
 
   # Run time dependencies
   s.add_runtime_dependency 'beaker', '> 3.0'