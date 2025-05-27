voxpupuli/beaker

commit addd9b320c09e27860da708f462635057c980c4c

BKR-160 Remove check for OS before issuing command, get_windows_command is not declared in scope

lib/beaker-rspec/helpers/serverspec.rb
@@ -137,12 +137,7 @@ module Specinfra
 
     def self.run(meth, *args)
       backend = Specinfra.backend
-      node = get_working_node
-      if node['platform'] =~ /windows/
-        cmd = Specinfra.command.get_windows_cmd(meth, *args)
-      else
-        cmd = Specinfra.command.get(meth, *args)
-      end
+      cmd = Specinfra.command.get(meth, *args)
       backend = Specinfra.backend
       ret = backend.run_command(cmd)
       if meth.to_s =~ /^check/

voxpupuli/beaker-rspec

commit 30a4c956f556c7c08fae15b886235c665d6d8a15

Remove unused get_windows_cmd method
Fixes: addd9b320c09 ("BKR-160 Remove check for OS before issuing command, get_windows_command is not declared in scope")

lib/beaker-rspec/helpers/serverspec.rb
@@ -86,31 +86,6 @@ module Specinfra::Helper::Os
   end
 end
 
-class Specinfra::CommandFactory
-  class << self
-    # Force creation of a windows command
-    def get_windows_cmd(meth, *args)
-      action, resource_type, subaction = breakdown(meth)
-      method =  action
-      method += "_#{subaction}" if subaction
-
-      common_class = Specinfra::Command
-      base_class = common_class.const_get(:Base)
-      os_class = common_class.const_get(:Windows)
-      version_class = os_class.const_get(:Base)
-      command_class = version_class.const_get(resource_type.to_camel_case)
-
-      command_class = command_class.create
-      unless command_class.respond_to?(method)
-        raise NotImplementedError,
-              "#{method} is not implemented in #{command_class}"
-      end
-
-      command_class.send(method, *args)
-    end
-  end
-end
-
 module Specinfra
   # Rewrite the runner to use the appropriate backend based upon platform information
   class Runner